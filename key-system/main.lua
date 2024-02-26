75587102cf77a2ce2480ec44d718ea5c8a10ef53
xurel <151750472+xurel7@users.noreply.github.com>
2024-02-25 19:59:18 -0500

    Update main.lua

--- key-system/main.lua
+++ key-system/main.lua
@@ -1,3 +1,5 @@
 if game.CreatorId == 11126716 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xurel7/solarhub/main/games/hoopslife.lua"))()
+elseif game.CreatorId == 1109656080 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/xurel7/solarhub/main/games/sl2.lua"))()
 end
