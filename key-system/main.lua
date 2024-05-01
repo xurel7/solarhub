abd2d542982a1d48680da8d20a70070340d560c4
xurel <151750472+xurel7@users.noreply.github.com>
2024-04-30 21:58:34 -0400

    Update main.lua

--- key-system/main.lua
+++ key-system/main.lua
@@ -15,6 +15,8 @@ elseif game.CreatorId == 16811615 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/xurel7/solarhub/main/games/hoopnation.lua"))()
 elseif game.PlaceId == 4581966615 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/xurel7/solarhub/main/games/anomic.lua"))()
+elseif game.CreatorId == 32333865 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/xurel7/solarhub/main/games/basketball-legends.lua"))()
 end
 
 if queue_on_teleport then
